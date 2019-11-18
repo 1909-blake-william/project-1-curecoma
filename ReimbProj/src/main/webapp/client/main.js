9985c2f5c9436ba1a39423d1142ed85aa1ec94d0
ctc <ctc@DESKTOP-DFR5CC8>
2019-11-18 10:42:57 -0500

    Http fixings

--- ReimbProj/src/main/webapp/client/main.js
+++ ReimbProj/src/main/webapp/client/main.js
@@ -177,39 +177,48 @@ function createReimb() {
 
     console.log('creating reimb');
 
-    let amount = document.getElementById('amount').value;
+    let amount = Math.round(document.getElementById('amount').value);
     let description = document.getElementById('description').value;
     let type = document.getElementById('type').value;
 
-    let reimbData = {
-        amount,
-        description,
-        type
-    };
+    if (description === false) {
+        description = " "
+    }
 
-    fetch('http://localhost:8080/ReimbProj/main', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        credentials: 'include', // put credentials: 'include' on every request to use session info //why
-        body: JSON.stringify(reimbData)
-    })
-        .then(resp => {
-            if (resp.status === 201) {
-                console.log('submit success')
-                document.getElementById('amount').value = '';
-                document.getElementById('description').value = "";
-                document.getElementById('error-message').innerText = 'Submission Success!';
-                setTimeout('', 3000);
-                document.getElementById('error-message').innerText = '';
-            } else {
-                document.getElementById('error-message').innerText = 'Submission failed!';
-                setTimeout('', 3000);
-                document.getElementById('error-message').innerText = '';
-            }
+    if (amount < 0) {
 
+        let reimbData = {
+            amount,
+            description,
+            type
+        };
+        document.getElementById('error-message').innerText = 'Submission Success!';
+
+        fetch('http://localhost:8080/ReimbProj/main', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            credentials: 'include', // put credentials: 'include' on every request to use session info //why
+            body: JSON.stringify(reimbData)
         })
+            .then(resp => {
+                if (resp.status === 201) {
+                    console.log('submit success')
+                    document.getElementById('amount').value = '';
+                    document.getElementById('description').value = "";
+                    setTimeout('', 3000);
+                    document.getElementById('error-message').innerText = '';
+                } else {
+                    document.getElementById('error-message').innerText = 'Submission failed!';
+                    setTimeout('', 3000);
+                    document.getElementById('error-message').innerText = '';
+                }
 
-    enterUser()
+            })
+
+        enterUser()
+    } else {
+        document.getElementById('error-message').innerText = 'Amount must be positive!';
+    }
 }/*POST*/
